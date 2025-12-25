b7cc99e554382908268ca35f8236dd8a1d995a5f
ragenmah <ragenmah99@gmail.com>
2025-12-25 15:15:44 +0545

    remove unnecessary files

--- js/home.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.tab').forEach(tab => {
-  tab.addEventListener('click', () => {
-    document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
-    tab.classList.add('active');
-  });
-});
